0c7e7721dfaf472714a8bca5f8a914b8a9548dd9
William Dubuc <2243756@cegepoutaouais.qc.ca>
2024-12-04 14:57:06 -0500

    Ajout navigation

--- app/src/main/java/com/williamd/objetconnecteapplication/MainActivity.kt
+++ app/src/main/java/com/williamd/objetconnecteapplication/MainActivity.kt
@@ -1,20 +1,50 @@
 package com.williamd.objetconnecteapplication
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.NonNull
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private val accueilFragment = AccueilFragment()
+    private val horaireFragment = HoraireFragment()
+    private val reglageFragment = HoraireFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+        bottomNavigationView.setOnNavigationItemSelectedListener(this)
+        bottomNavigationView.selectedItemId = R.id.accueil
+    }
+
+    override fun onNavigationItemSelected(@NonNull item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.accueil -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.flFragment, accueilFragment)
+                    .commit()
+                return true
+            }
+            R.id.horaire -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.flFragment, horaireFragment)
+                    .commit()
+                return true
+            }
+            R.id.reglages -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.flFragment, reglageFragment)
+                    .commit()
+                return true
+            }
         }
+        return false
     }
 }
